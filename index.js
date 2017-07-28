a8cb4e612981a176f9b151bde1dfa279d988eb97
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-07-28 09:49:00 -0400

    Remove attribution info from 'index.js'.

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-/**
- * MQ Vis. (Media Query Visualizer) is a JavaScript utility that allows developers to quickly 'visualize' one or more media queries.
- *
- * On initialization, MQ Vis. is given a series of objects containing media query information (media type, features, etc.).
- * MQ Vis. builds an HTML element and stylesheet for each query, and inserts both into the DOM.
- *
- * When a given query is satisfied, its associated HTML element will become visible within the viewport.
- *
- * @summary   MQ Vis. is a JavaScript utility that allows developers to quickly 'visualize' one or more media queries.
- *
- * @link      N/A
- * @since     0.0.0
- * @requires  N/A
- *
- * @author    Jesse R Mykolyn <jrmykolyn@gmail.com>
-*/
-
 // --------------------------------------------------
 // IMPORT MODULES
 // --------------------------------------------------
